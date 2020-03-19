c88d5dd4548aff58199349cad45dbf8232647da1
jirwincline <jci@pdx.edu>
2020-03-19 13:14:26 -0700

    it will start a messages component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { setUserPosts } from "../../actions";
+import firebase from "../../firebase";
+
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+import Message from "./Message";
+import Typing from "./Typing";
+import Skeleton from "./Skeleton";
 
 class Messages extends React.Component {
   render() {
-    return <div>Messages</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader
+          channelName={this.displayChannelName(channel)}
+          numUniqueUsers={numUniqueUsers}
+          handleSearchChange={this.handleSearchChange}
+          searchLoading={searchLoading}
+          isPrivateChannel={privateChannel}
+          handleStar={this.handleStar}
+          isChannelStarred={isChannelStarred}
+        />
+
+        <Segment>
+          <Comment.Group className="messages">
+            {this.displayMessagesSkeleton(messagesLoading)}
+            {searchTerm
+              ? this.displayMessages(searchResults)
+              : this.displayMessages(messages)}
+            {this.displayTypingUsers(typingUsers)}
+            <div ref={node => (this.messagesEnd = node)}></div>
+          </Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+          isPrivateChannel={privateChannel}
+          getMessagesRef={this.getMessagesRef}
+        />
+      </React.Fragment>
+    );
   }
 }
 
-export default Messages;
+export default connect(null, { setUserPosts })(Messages);
