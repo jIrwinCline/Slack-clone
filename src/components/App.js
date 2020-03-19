498d753b74aa2eb5331d37f3afb69b2dbc298e37
jirwincline <jci@pdx.edu>
2020-03-18 19:53:10 -0700

    it will style and add some functionality to the side panel of the page

--- src/components/App.js
+++ src/components/App.js
@@ -17,17 +17,43 @@ const App = ({
   secondaryColor
 }) => (
   <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
-    <ColorPanel />
-    <SidePanel />
+    <ColorPanel
+      key={currentUser && currentUser.name}
+      currentUser={currentUser}
+    />
+    <SidePanel
+      key={currentUser && currentUser.uid}
+      currentUser={currentUser}
+      primaryColor={primaryColor}
+    />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+        isPrivateChannel={isPrivateChannel}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
-      <MetaPanel />
+      <MetaPanel
+        key={currentChannel && currentChannel.name}
+        userPosts={userPosts}
+        currentChannel={currentChannel}
+        isPrivateChannel={isPrivateChannel}
+      />
     </Grid.Column>
   </Grid>
 );
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts,
+  primaryColor: state.colors.primaryColor,
+  secondaryColor: state.colors.secondaryColor
+});
+
+export default connect(mapStateToProps)(App);
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Menu } from "semantic-ui-react";
+
+import UserPanel from "./UserPanel";
+// import Starred from "./Starred";
+// import Channels from "./Channels";
+// import DirectMessages from "./DirectMessages";
 
 class SidePanel extends React.Component {
   render() {
-    return <div>SidePanel</div>;
+    const { currentUser, primaryColor } = this.props;
+
+    return (
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{ background: primaryColor, fontSize: "1.2rem" }}
+      >
+        <UserPanel primaryColor={primaryColor} currentUser={currentUser} />
+        {/* <Starred currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
+        <DirectMessages currentUser={currentUser} /> */}
+      </Menu>
+    );
   }
 }
 
