6b2392d2de47e86e119b762e3550fab91fa57c3f
jirwincline <jci@pdx.edu>
2020-03-19 15:03:24 -0700

    roll back messaage header to something more reasonable for now

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,35 +3,17 @@ import { Header, Segment, Input, Icon } from "semantic-ui-react";
 
 class MessagesHeader extends Component {
   render() {
-    const {
-      channelName,
-      numUniqueUsers,
-      handleSearchChange,
-      searchLoading,
-      isPrivateChannel,
-      handleStar,
-      isChannelStarred
-    } = this.props;
-
     return (
       <Segment clearing>
         <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
           <span>
-            {channelName}
-            {!isPrivateChannel && (
-              <Icon
-                onClick={handleStar}
-                name={isChannelStarred ? "star" : "star outline"}
-                color={isChannelStarred ? "yellow" : "black"}
-              />
-            )}
+            Channel
+            <Icon name={"star outline"} color="black" />
           </span>
-          <Header.Subheader>{numUniqueUsers}</Header.Subheader>
+          <Header.Subheader>2 Users</Header.Subheader>
         </Header>
         <Header floated="right">
           <Input
-            loading={searchLoading}
-            onChange={handleSearchChange}
             size="mini"
             icon="search"
             name="searchTerm"
