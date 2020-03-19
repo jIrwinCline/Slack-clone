3c5064e1d4978bf2e07a89f4d0e0a369739619a6
jirwincline <jci@pdx.edu>
2020-03-18 19:22:23 -0700

    added actions and reducers for users

--- src/index.js
+++ src/index.js
@@ -2,23 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
-
+import firebase from "./firebase";
+import Spinner from "./Spinner";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
 
+import rootReducer from "./reducers/index";
+
+import { setUser, clearUser } from "./actions";
+
 import "semantic-ui-css/semantic.min.css";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.setUser(user);
+        this.props.history.push("/");
+      } else {
+        this.props.history.push("/login");
+        this.props.clearUser();
+      }
+    });
+  }
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+  connect(mapStateToProps, { setUser, clearUser })(Root)
 );
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
