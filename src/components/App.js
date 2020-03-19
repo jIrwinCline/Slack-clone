ac5eee3ac3f606ad3345b7393817837e330cf41a
jirwincline <jci@pdx.edu>
2020-03-18 19:46:28 -0700

    begin to lay out main page in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
+import { Grid } from "semantic-ui-react";
 import "./App.css";
+import { connect } from "react-redux";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
+const App = ({
+  currentUser,
+  currentChannel,
+  isPrivateChannel,
+  userPosts,
+  primaryColor,
+  secondaryColor
+}) => (
+  <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
+    <ColorPanel />
+    <SidePanel />
+
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
 
 export default App;
