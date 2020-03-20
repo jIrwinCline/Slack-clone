8439350c7d1f35378215470a5d3902255246a170
jirwincline <jci@pdx.edu>
2020-03-19 17:48:04 -0700

    add search functions

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,10 @@ class Messages extends React.Component {
     messagesLoading: true,
     channel: this.props.currentChannel,
     user: this.props.currentUser,
-    numUniqueUsers: ""
+    numUniqueUsers: "",
+    searchTerm: "",
+    searchLoading: false,
+    searchResults: []
   };
 
   componentDidMount() {
@@ -38,11 +41,35 @@ class Messages extends React.Component {
       this.countUniqueUsers(loadedMessages);
     });
   };
-
   isProgressBarVisible = percent => {
     if (percent > 0) {
       this.setState({ progressBar: true });
     }
+  };
+  handleSearchChange = event => {
+    this.setState(
+      {
+        searchTerm: event.target.value,
+        searchLoading: true
+      },
+      () => this.handleSearchMessages()
+    );
+  };
+
+  handleSearchMessages = () => {
+    const channelMessages = [...this.state.messages];
+    const regex = new RegExp(this.state.searchTerm, "gi");
+    const searchResults = channelMessages.reduce((acc, message) => {
+      if (
+        (message.content && message.content.match(regex)) ||
+        message.user.name.match(regex)
+      ) {
+        acc.push(message);
+      }
+      return acc;
+    }, []);
+    this.setState({ searchResults });
+    setTimeout(() => this.setState({ searchLoading: false }), 1000);
   };
 
   countUniqueUsers = messages => {
@@ -76,7 +103,10 @@ class Messages extends React.Component {
       user,
       messages,
       progressBar,
-      numUniqueUsers
+      numUniqueUsers,
+      searchTerm,
+      searchResults,
+      searchLoading
     } = this.state;
 
     return (
@@ -84,13 +114,17 @@ class Messages extends React.Component {
         <MessagesHeader
           channelName={this.displayChannelName(channel)}
           numUniqueUsers={numUniqueUsers}
+          handleSearchChange={this.handleSearchChange}
+          searchLoading={searchLoading}
         />
 
         <Segment>
           <Comment.Group
             className={progressBar ? "messages__progress" : "messages"}
           >
-            {this.displayMessages(messages)}
+            {searchTerm
+              ? this.displayMessages(searchResults)
+              : this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
 
