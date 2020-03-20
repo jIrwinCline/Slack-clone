8b3e068ef9685722b948dd84b704f185604b981e
jirwincline <jci@pdx.edu>
2020-03-19 17:37:11 -0700

    edit header by channel and users

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,7 @@ class Messages extends React.Component {
     messagesLoading: true,
     channel: this.props.currentChannel,
     user: this.props.currentUser,
-    progressBar: false
+    numUniqueUsers: ""
   };
 
   componentDidMount() {
@@ -35,6 +35,7 @@ class Messages extends React.Component {
         messages: loadedMessages,
         messagesLoading: false
       });
+      this.countUniqueUsers(loadedMessages);
     });
   };
 
@@ -42,6 +43,18 @@ class Messages extends React.Component {
     if (percent > 0) {
       this.setState({ progressBar: true });
     }
+  };
+
+  countUniqueUsers = messages => {
+    const uniqueUsers = messages.reduce((acc, message) => {
+      if (!acc.includes(message.user.name)) {
+        acc.push(message.user.name);
+      }
+      return acc;
+    }, []);
+    const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+    const numUniqueUsers = `${uniqueUsers.length} user${plural ? "s" : ""}`;
+    this.setState({ numUniqueUsers });
   };
 
   displayMessages = messages =>
@@ -54,12 +67,24 @@ class Messages extends React.Component {
       />
     ));
 
+  displayChannelName = channel => (channel ? `${channel.name}` : "");
+
   render() {
-    const { messagesRef, channel, user, messages, progressBar } = this.state;
+    const {
+      messagesRef,
+      channel,
+      user,
+      messages,
+      progressBar,
+      numUniqueUsers
+    } = this.state;
 
     return (
       <React.Fragment>
-        <MessagesHeader />
+        <MessagesHeader
+          channelName={this.displayChannelName(channel)}
+          numUniqueUsers={numUniqueUsers}
+        />
 
         <Segment>
           <Comment.Group
